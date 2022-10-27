915fe437b722ae678603fb4e4003964757052a57
Thomas Sullivan <sullivan.t@gmail.com>
2022-10-27 09:30:28 -0400

    setup.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use egui::Checkbox;
+use egui_sdl2_gl as egui_backend;
+use egui_backend::sdl2::video::GLProfile;
+use egui_backend::{egui, gl, sdl2};
+use egui_backend::{sdl2::event::Event, DpiScaling, ShaderVersion};
+
+use std::time::Instant;
+use sdl2::video::SwapInterval;
+
 fn main() -> Result<(), String> {
     lib::go()?;
     Ok(())
