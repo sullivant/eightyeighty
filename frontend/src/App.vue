a2e818f974651635e8b0c62338f45b4b040633ab
Thomas Sullivan <thomas.sullivan@protedyne.com>
2024-04-29 16:26:45 -0400

    theming.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -140,7 +140,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <v-theme-provider theme="dark">
+  <v-theme-provider theme="light">
   <v-layout class="rounded rounded-md">
     <v-app-bar color="surface-variant" title="8080"></v-app-bar>
 
@@ -177,30 +177,6 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
