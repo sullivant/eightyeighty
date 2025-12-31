070363fc609533697e0049a22644a6e403c696d8
Thomas Sullivan <thomas@sullivanscientific.com>
2025-12-31 10:24:05 -0500

    Fixed inputs.asm so it latches.

--- resources/roms/inputs.asm
+++ resources/roms/inputs.asm
@@ -1,13 +1,38 @@
-        LXI     H, 8000h       ; HL points to memory location 8000h
+        ORG 0000h
 
-LOOP:   
-        IN      00h            ; Read input port 00h
-        ANI     01h            ; Mask all bits except bit 0 (coin 'c' pressed)
-        JNZ     NO_COIN        ; If nonzero, coin not pressed, skip increment 
+START:
+        LXI H, 8000h            ; Just a place to store coin cointer
+        MVI A, 00h
+        STA LATCH               ; latch = 0
 
-        MOV     A, M           ; Load value at (HL)
-        INR     A              ; Increment accumulator
-        MOV     M, A           ; Store back to memory at (HL)
+MAIN_LOOP:
+        IN  00h                 ; read input port 0
+        ANI 01h                 ; isolate coin bit (bit 0)
 
-NO_COIN:
-        JMP     LOOP           ; Repeat forever
+        JNZ COIN_UP             ; if bit == 1, coin released
+
+; --- coin bit is LOW (pressed) ---
+        LDA LATCH
+        ORA A
+        JNZ SKIP_INC            ; if already latched, skip
+
+        ; increment credit
+        MOV A, M
+        INR A
+        MOV M, A
+
+        ; set latch
+        MVI A, 01h
+        STA LATCH
+
+SKIP_INC:
+        JMP MAIN_LOOP
+
+; --- coin released ---
+COIN_UP:
+        MVI A, 00h
+        STA LATCH               ; clear latch
+        JMP MAIN_LOOP
+
+; --- data ---
+LATCH:  DB 00h
