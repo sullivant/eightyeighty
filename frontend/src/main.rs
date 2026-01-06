7aad802b33aa1f8f3c0e35e7ee9c3ca0d884df51
Thomas Sullivan <thomas@sullivanscientific.com>
2026-01-06 10:08:44 -0500

    layout info.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,19 @@
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
 
-    main_window.run()?;
+    main_window.on_cb_show_settings(|| {
+        let dialog = SettingsDialog::new().unwrap();
+        dialog.show().unwrap();
+    });
+
+    main_window.on_cb_exit(|| {
+        slint::quit_event_loop().unwrap();
+    });
+
+    main_window.show()?;
+    slint::run_event_loop()?;
 
     Ok(())
 }
