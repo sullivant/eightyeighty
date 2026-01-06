922613b5c92916788acbf77dda1b0ef2861e2e51
Thomas Sullivan <thomas@sullivanscientific.com>
2026-01-06 13:09:01 -0500

    integrated lucide with vscode, etc.

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,3 +1,18 @@
+use std::{collections::HashMap, path::PathBuf, fs};
+
 fn main() {
-    slint_build::compile("ui/main.slint").unwrap();
+    let out = PathBuf::from("ui/vendor/lucide.slint");
+    fs::create_dir_all("ui/vendor").unwrap();
+    fs::copy(lucide_slint::get_slint_file_path(), &out).unwrap();
+
+    println!("Copied lucide slint from {:?} to {:?}", lucide_slint::get_slint_file_path(), out);
+
+    let library = HashMap::from([
+        ("lucide".to_string(), out),
+    ]);
+
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
+ 
+    // Specify your Slint code entry here
+    slint_build::compile_with_config("ui/main.slint", config).expect("Slint build failed");
 }
