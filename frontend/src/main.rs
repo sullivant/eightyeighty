a991d3e284801656029087f1afe546aaab536a5e
Thomas Sullivan <thomas@sullivanscientific.com>
2026-01-06 16:04:14 -0500

    understanding globals with callbacks.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,18 +1,19 @@
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
 
-    main_window.on_cb_show_settings(|| {
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = MainWindow::new()?;
+
+    ui.global::<AppLogic>().on_cb_exit(|| {
+        slint::quit_event_loop().unwrap();
+    });
+
+    ui.global::<AppLogic>().on_cb_show_settings(|| {
         let dialog = SettingsDialog::new().unwrap();
         dialog.show().unwrap();
     });
 
-    main_window.on_cb_exit(|| {
-        slint::quit_event_loop().unwrap();
-    });
-
-    main_window.show()?;
+    ui.show()?;
     slint::run_event_loop()?;
 
     Ok(())
