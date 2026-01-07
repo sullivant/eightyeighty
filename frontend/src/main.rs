d7f6ea4d8a7c388908363a602a87ed0a14d22ec9
Thomas Sullivan <thomas@sullivanscientific.com>
2026-01-07 10:43:05 -0500

    understanding syncing better.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,94 @@
 slint::include_modules!();
 
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::time::Duration;
+
+// Allows for integration with the running system as if it was MidwayHardware.
+use emulator::bus::IoDevice;
+use emulator::devices::hardware::midway::MidwayHardware;
+use emulator::{self, Emulator};
+
+// A simple test rom with a few instructions to load at the start
+const ROM_TST: &[u8] = &[0x3E, 0x42, 0x76];
+
+
+struct HardwareProxy {
+    hardware: Rc<RefCell<MidwayHardware>>,
+}
+impl IoDevice for HardwareProxy {
+    fn input(&mut self, port: u8) -> u8 {
+        self.hardware.borrow_mut().input(port)
+    }
+    fn output(&mut self, port: u8, value: u8) {
+        self.hardware.borrow_mut().output(port, value)
+    }
+
+    fn set_port(&mut self, port: u8, value: u8) {
+        self.hardware.borrow_mut().set_port(port, value);
+    }
+
+    fn set_bit(&mut self, port: u8, bit: u8) {
+        self.hardware.borrow_mut().set_bit(port, bit);
+    }
+    fn clear_bit(&mut self, port: u8, bit: u8) {
+        self.hardware.borrow_mut().clear_bit(port, bit);
+    }
+}
+
+
+
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = MainWindow::new()?;
 
+    // Gather a connection to the MidwayHardware to be used in the EMU
+    let hardware = Rc::new(RefCell::new(MidwayHardware::new()));
+    println!("Original hardware Rc points to: {:p}", Rc::as_ptr(&hardware));
+
+    // Which is used when setting up the emu.
+    let emu = Rc::new(RefCell::new(setup_emu(&hardware)?));
+    let emu_for_ui = emu.clone();
+
+    // A handler to periodically sync various things in the UI with the current emu state
+    let ui_weak_sync = ui.as_weak();
+    let sync_timer = slint::Timer::default();
+    sync_timer.start(
+        slint::TimerMode::Repeated,
+        Duration::from_millis(100),
+        move || {
+            if let Some(ui) = ui_weak_sync.upgrade() {
+                ui.global::<EmulatorRegisters>().invoke_sync();
+            }
+        },
+    );
+
+    // Let's try handling a request for the CPU registers
+    let ui_weak_registers = ui.as_weak();
+    ui.global::<EmulatorRegisters>().on_sync(move || {
+        let Some(ui) = ui_weak_registers.upgrade() else {
+            return;
+        };
+
+        let emu = emu_for_ui.borrow();
+        let cpu = &emu.cpu;
+        println!("cpu.a: {:04X}", cpu.a);
+
+        let regs = ui.global::<EmulatorRegisters>();
+        regs.set_a(cpu.a as i32);
+        regs.set_b(cpu.b as i32);
+        regs.set_c(cpu.c as i32);
+        regs.set_e(cpu.e as i32);
+        regs.set_h(cpu.h as i32);
+        regs.set_l(cpu.l as i32);
+        regs.set_sp(cpu.sp as i32);
+        regs.set_pc(cpu.pc as i32);
+    });
+    // And sync it once.
+    ui.global::<EmulatorRegisters>().invoke_sync();
+
+
+    // Handle the request to cleanly exit the app
     ui.global::<AppLogic>().on_cb_exit(|| {
         slint::quit_event_loop().unwrap();
     });
@@ -17,3 +102,29 @@ fn main() -> Result<(), slint::PlatformError> {
 
     Ok(())
 }
+
+
+
+/// Will create the emulator machine, and insert the "default" ROM
+fn setup_emu(hardware: &Rc<RefCell<MidwayHardware>>) -> Result<Emulator, String> {
+    println!("Creating emulator...");
+    
+    // let hw_proxy = HardwareProxy { hardware: hardware.clone() };
+    // println!("HardwareProxy pointer before Box: {:p}", &*hw_proxy.hardware);
+    // let boxed_io: Box<dyn IoDevice> = Box::new(hw_proxy);
+
+    // println!("Box<dyn IoDevice> pointer before moving to Emulator:");
+    // let raw_ptr = &*boxed_io as *const dyn IoDevice;
+    // let (data_ptr, _vtable): (*const (), *const ()) = unsafe { std::mem::transmute(raw_ptr) };
+    // println!("data_ptr: {:p}", data_ptr);
+
+    // Box up the hardware proxy, with a cloned version of the hardware, and create an emu with it.
+    let mut emu = Emulator::with_io(Box::new(HardwareProxy { hardware: hardware.clone(),}));
+    // let mut emu = Emulator::with_io(boxed_io);
+
+    println!("Inserting ROM and loading...");
+    emu.load_rom(ROM_TST.to_vec())?;
+
+    Ok(emu)
+}
+
